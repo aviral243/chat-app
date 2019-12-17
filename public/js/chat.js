5f4022938ab229dc4a561d18f58a9a5ce44a3358
aviral243 <aviralgangwar24@gmail.com>
2019-12-18 01:45:27 +0530

    refactoring

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,32 @@
 const socket = io();
 
+//Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
+$messageFormInput.focus();
+
 socket.on("message", message => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", e => {
+$messageForm.addEventListener("submit", e => {
   e.preventDefault();
+  //disable
+
+  $messageFormButton.setAttribute("disabled", "disabled");
 
   const message = e.target.elements.message.value;
 
   socket.emit("sendMessage", message, error => {
+    //enable
+
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -17,10 +34,11 @@ document.querySelector("#message-form").addEventListener("submit", e => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+  $sendLocationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition(position => {
     socket.emit(
@@ -31,6 +49,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
       },
       () => {
         console.log("Location Shared!");
+        $sendLocationButton.removeAttribute("disabled");
       }
     );
   });
