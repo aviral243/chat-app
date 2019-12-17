c729ca2a740a305f755eda8c3c8ea7d5a8cf7590
aviral243 <aviralgangwar24@gmail.com>
2019-12-18 04:45:29 +0530

    Added timestamps for msgs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const Filter = require("bad-words");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const { generateMessage } = require("./utils/messages");
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -16,8 +17,8 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", socket => {
   console.log("New WebSocket connection.");
 
-  socket.emit("message", "Welcome");
-  socket.broadcast.emit("message", "A new user has joined!");
+  socket.emit("message", generateMessage("Welcome!"));
+  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
@@ -25,7 +26,7 @@ io.on("connection", socket => {
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed");
     }
-    io.emit("message", message);
+    io.emit("message", generateMessage(message));
 
     callback();
   });
@@ -39,7 +40,7 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat!");
+    io.emit("message", generateMessage("A user has left the chat!"));
   });
 });
 
