d63b769e92dee60d9bb4f53b93fc51dd71eb49d7
aviral243 <aviralgangwar24@gmail.com>
2019-12-18 23:13:01 +0530

    remove username

--- src/index.js
+++ src/index.js
@@ -69,7 +69,14 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("A user has left the chat!"));
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.username} has left the chat`)
+      );
+    }
   });
 });
 
